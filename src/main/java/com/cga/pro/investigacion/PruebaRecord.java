286593cd2db389eb76cf3906cfc1fd16e0dc3f59
Ivan <javaspringcga@gmail.com>
2023-08-25 18:48:55 +0100

    Continuamos igual

--- src/main/java/com/cga/pro/investigacion/PruebaRecord.java
+++ src/main/java/com/cga/pro/investigacion/PruebaRecord.java
@@ -4,8 +4,9 @@ public class PruebaRecord {
 
     public static void main(String[] args) {
 
-        Registro reg = new Registro("Ivan", 54);
+        Registro reg = new Registro("Ivan Rodriguez Saiz", 54);
 
         System.out.println(reg);
+        System.out.println(reg.hashCode());
     }
 }
