d20b84182084977a2001ea726efd7003eeb96f57
Ivan <javaspringcga@gmail.com>
2023-08-25 18:33:27 +0100

    Se agregaron 2 clases nuevas al paquete Investigacion

---
+++ src/main/java/com/cga/pro/investigacion/PruebaRecord.java
@@ -0,0 +1,11 @@
+package com.cga.pro.investigacion;
+
+public class PruebaRecord {
+
+    public static void main(String[] args) {
+
+        Registro reg = new Registro("Ivan", 54);
+
+        System.out.println(reg);
+    }
+}
---
+++ src/main/java/com/cga/pro/investigacion/Registro.java
@@ -0,0 +1,5 @@
+package com.cga.pro.investigacion;
+
+public record Registro(String name, int age) {
+
+}
