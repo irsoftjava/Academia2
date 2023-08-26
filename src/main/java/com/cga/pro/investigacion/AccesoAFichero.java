70aebef72563b82af1e47786395bbec6e1bcae24
irsoftjava <javaspringcga@gmail.com>
2023-08-26 22:52:48 +0100

    Se agregaron dos archivos a la carpeta 'investigacion', AccesoAFichero.java y archivo.txt

---
+++ src/main/java/com/cga/pro/investigacion/AccesoAFichero.java
@@ -0,0 +1,33 @@
+package com.cga.pro.investigacion;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AccesoAFichero {
+
+    static class LeerFichero {
+        public void lee() {
+            try {
+                FileReader entrada = new FileReader(
+                        "./src/main/java/com/cga/pro/investigacion/archivo.txt");
+
+                //int c = entrada.read();
+                int c = 0;
+
+                while (c != -1) {
+                    c = entrada.read();
+                    char letra = (char) c;
+                    System.out.printf("%c", letra);
+                }
+
+            } catch (IOException e) {
+                System.out.println("Error: Archivo no encontrado...");
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        LeerFichero accediendo = new LeerFichero();
+        accediendo.lee();
+    }
+}
