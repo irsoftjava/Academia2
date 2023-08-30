43e6da71005b67f3b1c44677a48397a253c9a151
Ivan <javaspringcga@gmail.com>
2023-08-30 20:12:41 +0100

    Se agrego el paquete oop2 y dentro la clase Aeropuerto y el archivo AppAeropuerto.java.

---
+++ src/main/java/com/cga/oop2/Aeropuerto.java
@@ -0,0 +1,112 @@
+package com.cga.oop2;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Aeropuerto {
+
+    // Atributos
+    private String nombre;
+    private String pais;
+    private String calle;
+    private Integer numero;
+    private String ciudad;
+    private Integer anioInauguracion;
+    private Integer capacidad;
+
+    // Constructor vacío
+    public Aeropuerto() {
+    }
+
+    // Constructor parametrizado
+    public Aeropuerto(String nombre, String pais, String calle, Integer numero, String ciudad, Integer anioInauguracion, Integer capacidad) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+        this.anioInauguracion = anioInauguracion;
+        this.capacidad = capacidad;
+    }
+
+    // Métodos
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Integer getAnioInauguracion() {
+        return anioInauguracion;
+    }
+
+    public void setAnioInauguracion(Integer anioInauguracion) {
+        this.anioInauguracion = anioInauguracion;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public Integer aniosAbierto() {
+        int anios = 0;
+
+        Calendar fecha = new GregorianCalendar();
+        int anio = fecha.get(Calendar.YEAR);
+        anios = anio - anioInauguracion;
+
+        return anios;
+    }
+
+    @Override
+    public String toString() {
+        return "Aeropuerto(" +
+                "nombre='" + nombre + '\'' +
+                ", pais='" + pais + '\'' +
+                ", calle='" + calle + '\'' +
+                ", numero=" + numero +
+                ", ciudad='" + ciudad + '\'' +
+                ", anioInauguracion=" + anioInauguracion +
+                ", capacidad=" + capacidad +
+                ')';
+    }
+}
---
+++ src/main/java/com/cga/oop2/AppAeropuerto.java
@@ -0,0 +1,14 @@
+package com.cga.oop2;
+
+public class AppAeropuerto {
+
+    public static void main(String[] args) {
+
+        Aeropuerto ae1 = new Aeropuerto("Ciudad de la Laguna", "España",
+                "Carretera del Norte", 210, "San Cristóbal de la Laguna",
+                1946, 876);
+
+        System.out.println(ae1);
+        System.out.println("El Aeropuerto " + ae1.getNombre() + " tiene " + ae1.aniosAbierto() + " años abierto.");
+    }
+}
