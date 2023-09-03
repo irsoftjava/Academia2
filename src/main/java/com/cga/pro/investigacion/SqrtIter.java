3bf38e9d5c5c1af8914cfe9f9261353645bdd9eb
irsoftjava <javaspringcga@gmail.com>
2023-09-03 09:13:13 +0100

    Se agregó el archivo SqrtIter.java al paquete investigacion.

---
+++ src/main/java/com/cga/pro/investigacion/SqrtIter.java
@@ -0,0 +1,21 @@
+package com.cga.pro.investigacion;
+
+public class SqrtIter {
+
+    public static Double sqrtIter(Double guess, Double x) {
+        return (isGoodEnough(guess, x)) ? guess : sqrtIter(inprove(guess, x), x);
+    }
+
+    public static Boolean isGoodEnough(Double guess, Double x) {
+        return Math.abs(guess * guess - x ) < 0.001;
+    }
+
+    public static Double inprove(Double guess, Double x) {
+        return (guess + x / guess) / 2;
+    }
+
+    public static void main(String[] args) {
+        Double a = sqrtIter(1.0, 54.0);
+        System.out.println(a);
+    }
+}
