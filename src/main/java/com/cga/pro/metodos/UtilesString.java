03a5c9271159319458fe20d3e2fafc7e37502b59
Ivan <javaspringcga@gmail.com>
2023-09-04 15:47:55 +0100

    Se actualizó el archivo UtilesString.java

--- src/main/java/com/cga/pro/metodos/UtilesString.java
+++ src/main/java/com/cga/pro/metodos/UtilesString.java
@@ -3,12 +3,7 @@ package com.cga.pro.metodos;
 public class UtilesString {
 
     public boolean esVocal(char c) {
-        // if ("aeiou".contains(String.valueOf(c).toLowerCase())) {
-        //    return true;
-        // }
-        // return false;
-
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    
+        return "aeiouAEIOU".contains(String.valueOf(c));
     }
 }
