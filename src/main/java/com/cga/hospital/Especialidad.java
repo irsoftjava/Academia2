00b8518bf3e5253732cece8582d80930b8b24cec
irsoftjava <javaspringcga@gmail.com>
2023-09-06 12:55:56 +0100

    Se documentaron los enums Especialidad y Oficio.

--- src/main/java/com/cga/hospital/Especialidad.java
+++ src/main/java/com/cga/hospital/Especialidad.java
@@ -1,5 +1,8 @@
 package com.cga.hospital;
 
+/**
+ * Enum Especialidad - describe algunas especialidades sanitarias
+ */
 public enum Especialidad {
     OFTALMOLOGIA, DERMATOLOGIA, PSICOLOGIA, ORTOPEDIA, PSIQUIATRIA, NEO_NATAL, PEDIATRIA, ENFERMERIA
 }
--- src/main/java/com/cga/hospital/Oficio.java
+++ src/main/java/com/cga/hospital/Oficio.java
@@ -1,5 +1,8 @@
 package com.cga.hospital;
 
+/**
+ * Enum Oficio - describe algunos tipo de oficios no sanitarios
+ */
 public enum Oficio {
     AUX_LIMPIEZA, ELECTRICISTA, CARPINTERO, FONTANERO
 }
