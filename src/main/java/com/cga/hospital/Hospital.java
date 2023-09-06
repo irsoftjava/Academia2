61911c0be9c7d830dd77a56e33ea48619b129b32
irsoftjava <javaspringcga@gmail.com>
2023-09-06 13:29:50 +0100

    Se documentó la clase Hospital.

--- src/main/java/com/cga/hospital/Hospital.java
+++ src/main/java/com/cga/hospital/Hospital.java
@@ -2,22 +2,46 @@ package com.cga.hospital;
 
 import java.util.ArrayList;
 
+/**
+ * Clase Hospital
+ */
 public class Hospital {
 
     private String nombre;
     private Direccion direccion;
     private ArrayList<Persona> personal;
 
+    /**
+     * Constructor clase Hospital
+     *
+     * <p>La clase Hospital usa una variable privada <i> ArrayList<Persona> personal</i></p> para almacenar al
+     * personal Sanitario y NoSanitario.
+     *
+     * @param nombre nombre del Hospital, de tipo String
+     * @param direccion direccion del Hospital, de tipo Direccion
+     * @see Direccion
+     * @see Persona
+     * @author Ivan Rodriguez Saiz
+     */
     public Hospital(String nombre, Direccion direccion) {
         this.nombre = nombre;
         this.direccion = direccion;
         personal = new ArrayList<>();
     }
 
+    /**
+     * add - Agrega a la colleccion <i>ArrayList<Persona> personal</i> la variable p de tipo Persona
+     * @param p de tipo Persona
+     * @see Persona
+     */
     public void add(Persona p) {
         personal.add(p);
     }
 
+    /**
+     * showPersonal - Muestra por consola todo el personal del Hospital
+     * tanto Sanitarios como NoSanitarios
+     */
     public void showPersonal() {
         System.out.println();
         if (!personal.isEmpty()) {
