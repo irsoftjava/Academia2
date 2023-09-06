31317ac6d25fab1f4a14ebda85de25c77be0f47d
Ivan <javaspringcga@gmail.com>
2023-09-06 16:07:29 +0100

    Se corrigió la documentación de la clase Hospital.java

--- src/main/java/com/cga/hospital/Hospital.java
+++ src/main/java/com/cga/hospital/Hospital.java
@@ -14,8 +14,8 @@ public class Hospital {
     /**
      * Constructor clase Hospital
      *
-     * <p>La clase Hospital usa una variable privada <i> ArrayList<Persona> personal</i></p> para almacenar al
-     * personal Sanitario y NoSanitario.
+     * <p>La clase Hospital usa una variable privada <i>ArrayList  personal de tipo Persona</i>
+     * para almacenar al personal Sanitario y NoSanitario.
      *
      * @param nombre nombre del Hospital, de tipo String
      * @param direccion direccion del Hospital, de tipo Direccion
@@ -30,7 +30,7 @@ public class Hospital {
     }
 
     /**
-     * add - Agrega a la colleccion <i>ArrayList<Persona> personal</i> la variable p de tipo Persona
+     * add - Agrega a la colleccion <i>ArrayList de tipo Persona la variable p de tipo Persona</i>
      * @param p de tipo Persona
      * @see Persona
      */
