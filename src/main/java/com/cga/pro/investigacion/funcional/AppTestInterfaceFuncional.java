86dcdab74acde5f017687bdba919dadef794f3ec
irsoftjava <javaspringcga@gmail.com>
2023-09-08 11:54:47 +0100

    Se modificó el la clase AppTestInterfaceFuncional y se agregó Pickachu.

--- src/main/java/com/cga/pro/investigacion/funcional/AppTestInterfaceFuncional.java
+++ src/main/java/com/cga/pro/investigacion/funcional/AppTestInterfaceFuncional.java
@@ -14,7 +14,7 @@ public class AppTestInterfaceFuncional {
     }
 
     public static void doSomethingAnonima() {
-        // Clase anónima
+        // Implementación basada en una Clase anónima
         MyFunctionalInterface duke = new MyFunctionalInterface() {
             @Override
             public String doSomeThing(String param) {
@@ -26,19 +26,35 @@ public class AppTestInterfaceFuncional {
     }
 
     public static void doSomethingFuncional() {
-
+        // Implementación basada en Lambda
         MyFunctionalInterface clippy = (String param) -> {
             return "Hola soy Clippy, y recibí el siguiente parámetro: " + param;
         };
 
-        System.out.println(clippy.doSomeThing("Parámetro Funcional."));
+        MyFunctionalInterface wilbert = (p) -> "Hola soy Wilbert, y recibí el siguiente parámetro: " + p;
+
+        doSomethingHighOrder(clippy);
+        doSomethingHighOrder(wilbert);
+        doSomethingHighOrder(x -> "Hola soy anonymus, y recibí el siguiente parámetro: " + x);
+
+        System.out.println();
+        var pickachu = new Pickachu();
+        doSomethingHighOrder(pickachu::pika);
+        doSomethingHighOrder(pickachu::impactrueno);
+        //System.out.println(clippy.doSomeThing("Parámetro Funcional."));
+    }
+
+    public static void doSomethingHighOrder(MyFunctionalInterface comportamiento) {
+        // Ejecuto el comportamiento que recibí como argumento
+        var respuesta = comportamiento.doSomeThing("Java 11 es Genial.");
+        System.out.println(respuesta);
     }
 
     public static void main(String[] args) {
 
         System.out.println();
-        doSomethingTraditional();
-        doSomethingAnonima();
+        //doSomethingTraditional();
+        //doSomethingAnonima();
         doSomethingFuncional();
     }
 }
---
+++ src/main/java/com/cga/pro/investigacion/funcional/Pickachu.java
@@ -0,0 +1,12 @@
+package com.cga.pro.investigacion.funcional;
+
+public class Pickachu {
+
+    String pika(String param) {
+        return "Pika pika " + param;
+    }
+
+    String impactrueno(String param) {
+        return "Pikaaaaaaaaaaaaaa " + param;
+    }
+}
