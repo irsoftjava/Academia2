2e0b176669af0f04b65661d71f64afb800749a59
Ivan <javaspringcga@gmail.com>
2023-09-08 19:42:59 +0100

    Se actualiza la clase Main.

---
+++ src/main/java/com/cga/oop9/Main.java
@@ -0,0 +1,11 @@
+package com.cga.oop9;
+
+public class Main implements Animal {
+
+    public static void main(String[] args) {
+
+        Rana r = new Rana(12.5, 0.85, "Rojo", "Cabezona", true);
+
+        r.observar();
+    }
+}
