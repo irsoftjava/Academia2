391a86c5a8665a36d1ccd841dea455e8fd3e1125
irsoft <ivanrsaiz@gmail.com>
2023-09-17 16:21:34 +0100

    Se modificaron la clase Main.

--- src/main/java/com/cga/pro/investigacion/openbookcamp/funcional/Main.java
+++ src/main/java/com/cga/pro/investigacion/openbookcamp/funcional/Main.java
@@ -1,28 +1,32 @@
 package com.cga.pro.investigacion.openbookcamp.funcional;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.SortedMap;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
 
-    public static String toMayuscula (String nombre){
+    public static String toMayuscula(String nombre) {
         return nombre.toUpperCase();
     }
 
-    public static String diHola (String nombre){
+    public static String diHola(String nombre) {
         return "Hola " + nombre;
     }
-    public static int suma ( int a, int b){
+
+    public static int suma(int a, int b) {
         return a + b;
     }
 
     public static void main(String[] args) {
 
-        int result = suma(1,2);
-        int result2 = suma(2, 1);
-
-        System.out.println(result + " " + result2);
-        System.out.println(diHola("Roger"));
+//        int result = suma(1,2);
+//        int result2 = suma(2, 1);
+//
+//        System.out.println(result + " " + result2);
+//        System.out.println(diHola("Roger"));
 
         Funcionales f = new Funcionales();
         f.pruebas();
@@ -32,7 +36,18 @@ public class Main {
         nombres.add("Pepe");
         nombres.add("Juan");
 
-        Stream<String> valores = nombres.stream().map(x -> "Hola: " + x.toUpperCase());
+        Stream<String> valores = nombres.stream()
+                .map(x -> x.toUpperCase())
+                .filter(x -> x.startsWith("P"));
+
         valores.forEach(x -> System.out.println(x));
+
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        var stNumeros = Arrays.stream(numeros);
+        var resultado = stNumeros.filter(x -> x % 2 == 0).reduce(0, (x, y) -> {
+            System.out.println("X es " + x + " Y es " + y);
+            return x + y;
+        });
+        System.out.println(resultado);
     }
 }
