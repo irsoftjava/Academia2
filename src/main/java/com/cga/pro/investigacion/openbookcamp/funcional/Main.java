37df1115f8c2e1f1c165188e03a495fed10eac1d
irsoft <ivanrsaiz@gmail.com>
2023-09-17 16:26:27 +0100

    Se modificaron la clase Main.

--- src/main/java/com/cga/pro/investigacion/openbookcamp/funcional/Main.java
+++ src/main/java/com/cga/pro/investigacion/openbookcamp/funcional/Main.java
@@ -2,19 +2,17 @@ package com.cga.pro.investigacion.openbookcamp.funcional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.SortedMap;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
 
-    public static String toMayuscula(String nombre) {
-        return nombre.toUpperCase();
-    }
-
-    public static String diHola(String nombre) {
-        return "Hola " + nombre;
-    }
+//    public static String toMayuscula(String nombre) {
+//        return nombre.toUpperCase();
+//    }
+//
+//    public static String diHola(String nombre) {
+//        return "Hola " + nombre;
+//    }
 
     public static int suma(int a, int b) {
         return a + b;
@@ -47,10 +45,8 @@ public class Main {
         var resultado = stNumeros
                 .map( x -> x * 2)
                 .filter(x -> x % 2 == 0)
-                .reduce(0, (x, y) -> {
-            System.out.println("X es " + x + " Y es " + y);
-            return x + y;
-        });
+                .reduce(0, (x, y) -> x + y);
+
         System.out.println(resultado);
     }
 }
